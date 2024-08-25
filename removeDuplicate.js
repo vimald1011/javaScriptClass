34ae34225cbaab845bd12e55f87b61e5a475a07b
vimal dubey <vimald1011@gmail.com>
2024-08-25 07:08:50 +0530

    using for and for of loop

--- removeDuplicate.js
+++ removeDuplicate.js
@@ -1,4 +1,18 @@
-const array = [11, 3, 4, 11, 4, 7, 3];
-const setArray = new Set(array);
-const newArray = [...setArray];
+const array = [11, 3, 4, 11, 4, 7, 3, 5, 34, 33, 5, 3];
+// const setArray = new Set(array);
+// const newArray = [...setArray];
+// console.log(newArray);
+const newArray = [];
+
+// for (let index = 0; index < array.length; index++) {
+//     if (!newArray.includes(array[index])) {
+//         newArray.push(array[index])
+//     }
+// }
+for (const element of array) {
+    if (!newArray.includes(element)) {
+        newArray.push(element);
+    }
+}
 console.log(newArray);
+
